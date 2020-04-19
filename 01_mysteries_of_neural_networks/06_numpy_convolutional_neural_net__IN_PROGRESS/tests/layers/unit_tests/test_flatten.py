3d89fb52f868ced5c6620038cade467eead9b88c
SkalskiP <piotr.skalski92@gmail.com>
2020-04-20 00:06:14 +0200

    more flatten layer test

--- 01_mysteries_of_neural_networks/06_numpy_convolutional_neural_net__IN_PROGRESS/tests/layers/unit_tests/test_flatten.py
+++ 01_mysteries_of_neural_networks/06_numpy_convolutional_neural_net__IN_PROGRESS/tests/layers/unit_tests/test_flatten.py
@@ -26,3 +26,27 @@ class TestFlattenLayer:
 
         # then
         assert result.shape == (28 * 28 * 3, 100)
+
+    def test_flatten_backward_pass_with_tree_axis(self):
+        # given
+        activation = np.random.rand(28, 28, 100)
+
+        # when
+        flatten_layer = FlattenLayer()
+        forward_result = flatten_layer.forward_pass(activation)
+        backward_result = flatten_layer.backward_pass(forward_result)
+
+        # then
+        assert np.alltrue(activation == backward_result)
+
+    def test_flatten_backward_pass_with_four_axis(self):
+        # given
+        activation = np.random.rand(28, 28, 3, 100)
+
+        # when
+        flatten_layer = FlattenLayer()
+        forward_result = flatten_layer.forward_pass(activation)
+        backward_result = flatten_layer.backward_pass(forward_result)
+
+        # then
+        assert np.alltrue(activation == backward_result)
