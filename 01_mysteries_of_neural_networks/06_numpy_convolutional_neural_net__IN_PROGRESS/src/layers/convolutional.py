e0f5a5411ad04a08255f2a9fa1af9d18d065044d
SkalskiP <piotr.skalski92@gmail.com>
2020-04-28 23:57:27 +0200

    up

--- 01_mysteries_of_neural_networks/06_numpy_convolutional_neural_net__IN_PROGRESS/src/layers/convolutional.py
+++ 01_mysteries_of_neural_networks/06_numpy_convolutional_neural_net__IN_PROGRESS/src/layers/convolutional.py
@@ -1,216 +1,92 @@
 from __future__ import annotations
 
-from typing import Tuple
+from typing import Tuple, Optional
 
 import numpy as np
 
 from src.base import Layer
-from src.errors import InvalidPaddingModeError
 
 
 class ConvLayer2D(Layer):
-    def __init__(self, W: np.array, b: np.array, padding: str = 'valid'):
+    def __init__(
+        self, w: np.array,
+        b: np.array,
+        padding: str = 'valid',
+        stride: int = 2
+    ):
         """
-        f - filter count
-        fd - filter dimension
-        c - number of channels
-        :param W - filter weights array with (f, fd, fd, c) shape
-        :param b - filter bias array with (f, ) shape
+        :param w -  4D tensor with shape (h_f, w_f, c_f, n_f)
+        :param b - 1D tensor with shape (n_f, )
         :param padding - flag describing type of activation padding valid/same
+        :param stride - stride along width and height of input volume used to
+        ------------------------------------------------------------------------
+        h_f - height of filter volume
+        w_f - width of filter volume
+        c_f - number of channels of filter volume
+        n_f - number of filters in filter volume
         """
-        self._W, self._b = W, b
+        self._w, self._b = w, b
         self._padding = padding
-        self._dW, self._db = None, None
+        self._stride = stride
+        self._dw, self._db = None, None
         self._A = None
 
     @classmethod
     def initialize(
-        cls,
-        filters: int,
+        cls, filters: int,
         kernel_shape: Tuple[int, int, int],
-        padding: str = 'valid'
+        padding: str = 'valid',
+        stride: int = 2
     ) -> ConvLayer2D:
-        W = np.random.randn(filters, *kernel_shape) * 0.1
+        w = np.random.randn(*kernel_shape, filters) * 0.1
         b = np.random.randn(filters) * 0.1
-        return cls(W=W, b=b, padding=padding)
+        return cls(w=w, b=b, padding=padding, stride=stride)
 
-    def forward_pass_slow(self, activation: np.array) -> np.array:
+    @property
+    def weights(self) -> Optional[Tuple[np.array, np.array]]:
+        return self._w, self._b
+
+    @property
+    def gradients(self) -> Optional[Tuple[np.array, np.array]]:
+        if self._dw is None or self._db is None:
+            return None
+        return self._dw, self._db
+
+    def forward_pass(self, a_prev: np.array) -> np.array:
         """
-        N - number of items
-        ad - activation dimension
-        c - number of channels
-        :param activation - activation array with (ad, ad, c, N) shape
+        :param a_prev - 4D tensor with shape(n, h, w, c)
+        :output 4D tensor with shape(n, h, w, n_f)
+        ------------------------------------------------------------------------
+        n - number of examples in batch
+        w - width of input volume
+        h - width of input volume
+        c - number of channels of the input volume
+        n_f - number of filters in filter volume
         """
-        self._A = np.array(activation, copy=True)
+        pass
 
-        _, fd, _, _ = self._W.shape
-
-        output_shape = ConvLayer2D.calculate_forward_pass_output_shape(
-            activation=activation,
-            W=self._W,
-            mode=self._padding
-        )
-
-        Z = np.zeros(shape=output_shape)
-
-        activation_pad = ConvLayer2D.pad_activation(
-            activation=activation,
-            fd=fd,
-            mode=self._padding
-        )
-
-        height, width, channels, items = output_shape
-        for i in range(items):
-            for h in range(height):
-                for w in range(width):
-                    for c in range(channels):
-                        filter_W = self._W[c, :, :, :]
-                        filter_b = self._b[c]
-                        activation_slice = activation_pad[h:h+fd, w:w+fd, :, i]
-                        Z[h, w, c, i] = ConvLayer2D.single_convolution_step(
-                            activation_slice=activation_slice,
-                            filter_W=filter_W,
-                            filter_b=filter_b
-                        )
-        return Z
-
-    def forward_pass(self, activation: np.array) -> np.array:
-        self._A = np.array(activation, copy=True)
-
-        _, fd, _, _ = self._W.shape
-
-        output_shape = ConvLayer2D.calculate_forward_pass_output_shape(
-            activation=activation,
-            W=self._W,
-            mode=self._padding
-        )
-
-        Z = np.zeros(shape=output_shape)
-
-        activation_pad = ConvLayer2D.pad_activation(
-            activation=activation,
-            fd=fd,
-            mode=self._padding
-        )
-
-        height, width, channels, items = output_shape
-
-        for h in range(height):
-            for w in range(width):
-                Z[h, w, :, :] = np.sum(
-                    activation_pad[np.newaxis, h:h+fd, w:w+fd, :, :] *
-                    self._W[:, :, :, :, np.newaxis], axis=(1, 2, 3))
-        return Z
-
-    def backward_pass(self, activation: np.array) -> np.array:
-        _, fd, _, _ = self._W.shape
-        ad, _, _, _ = self._A.shape
-
-        self._dW = np.zeros_like(self._W)
-        self._db = np.zeros_like(self._b)
-        dA = np.zeros_like(self._A)
-
-        A_pad = ConvLayer2D.pad_activation(
-            activation=self._A,
-            fd=fd,
-            mode=self._padding
-        )
-
-        ad_pad, _, _, _ = A_pad.shape
-        pad = (ad_pad - ad) // 2
-
-        dA_pad = np.zeros_like(A_pad)
-
-        height, width, channels, items = self._A.shape
-
-        for i in range(items):
-            for c in range(channels):
-                self._b[c] = activation[:, :, c, :].sum()
-                for h in range(height + 2 * pad - fd + 1):
-                    for w in range(width + 2 * pad - fd + 1):
-                        A_pad_slice = A_pad[h:h+fd, w:w+fd, :, i]
-                        self._dW[c, :, :, :] = A_pad_slice * activation[h, w, c, i]
-                        dA_pad[h:h+fd, w:w+fd, :, i] += self._W[c, :, :, :] * activation[h, w, c, i]
-            dA[:, :, :, i] = dA_pad[pad:pad+ad, pad:pad+ad, :, i]
-
-        return dA
-
-    def update(self, lr: float) -> None:
-        self._W -= lr * self._dW
-        self._b -= lr * self._db
-
-    @staticmethod
-    def calculate_forward_pass_output_shape(
-        activation: np.array,
-        W: np.array,
-        mode: str
-    ) -> Tuple[int, int, int, int]:
+    def backward_pass(self, da_curr: np.array) -> np.array:
         """
-        N - number of items
-        f - filter count
-        fd - filter dimension
-        ad - activation dimension
-        c - number of channels
-        :param activation - activation array with (ad, ad, c, N) shape
-        :param W - filter weights array with (f, fd, fd, c) shape
-        :param mode - flag describing type of activation padding valid/same
-        for example convolution with padding in valid mode, filter shape equal
-        to (5, 5, 9), filter count equal to 15 and activation shape equal to
-        (11, 11, 9, 64) will produce output with (7, 7, 15, 64)
+        :param da_curr - 4D tensor with shape(n, h, w, n_f)
+        :output 4D tensor with shape(n, h, w, c)
+        ------------------------------------------------------------------------
+        n - number of examples in batch
+        w - width of input volume
+        h - width of input volume
+        c - number of channels of the input volume
+        n_f - number of filters in filter volume
         """
-        f, fd, _, _ = W.shape
-        ad, _, _, N = activation.shape
-        if mode == 'valid':
-            output_dim = ad - fd + 1
-            return output_dim, output_dim, f, N
-        elif mode == 'same':
-            return ad, ad, f, N
-        else:
-            raise InvalidPaddingModeError(
-                f"The padding value can only be equal to valid or same, "
-                f"got {mode} instead.")
+        pass
 
-    @staticmethod
-    def pad_activation(
-        activation: np.array,
-        fd: int,
-        mode: str
-    ) -> np.array:
+    def set_wights(self, w: np.array, b: np.array) -> None:
         """
-        N - number of items
-        ad - activation dimension
-        c - number of channels
-        :param activation - activation array with (ad, ad, c, N) shape
-        :param fd - filter dimension
-        :param mode - flag describing type of activation padding valid/same
-        for example padding in same mode with filter dimension equal to 5 will
-        change activation shape from (11, 11, 9, 64) to (15, 15, 9, 64)
+        :param w -  4D tensor with shape (h_f, w_f, c_f, n_f)
+        :param b - 1D tensor with shape (n_f, )
+        ------------------------------------------------------------------------
+        h_f - height of filter volume
+        w_f - width of filter volume
+        c_f - number of channels of filter volume
+        n_f - number of filters in filter volume
         """
-        if mode == 'valid':
-            return activation
-        elif mode == 'same':
-            pad = fd // 2
-            return np.pad(
-                array=activation,
-                pad_width=((pad, pad), (pad, pad), (0, 0), (0, 0)),
-                mode='constant'
-            )
-        else:
-            raise InvalidPaddingModeError(
-                f"The padding value can only be equal to valid or same, "
-                f"got {mode} instead.")
-
-    @staticmethod
-    def single_convolution_step(
-        activation_slice: np.array,
-        filter_W: np.array,
-        filter_b: float
-    ) -> np.array:
-        """
-        fd - filter dimension
-        c - number of channels
-        :param activation_slice - slice of layer activation array with (fd, fd, c) shape
-        :param filter_W - single filter weights array with (fd, fd, c) shape
-        :param filter_b - single filter bias
-        """
-        return np.sum(np.multiply(activation_slice, filter_W)) + filter_b
+        self._w = w
+        self._b = b
